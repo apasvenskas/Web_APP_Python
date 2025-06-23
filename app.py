4e4e8b3ad2a0a0943b4845c9959f3f026f81281b
Audrius <apasvenskas@outlook.com>
2025-06-22 18:33:32 -0700

    storing user data

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
@@ -23,8 +24,14 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
         print(first_name, last_name, email, date, occupation)
+
+        form = Form(first_name=first_name, last_name=last_name, date=date_obj, email=email, occupation=occupation)
+        # SQL insert
+        db.session.add(form)
+        db.session.commit()
 
     return render_template("index.html")
 
